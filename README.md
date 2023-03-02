21bb9f6c46523f7cd0d3755db11ecdf729892a5c
Anastasiaqq <anastasiyaberezina610@gmail.com>
2023-03-02 12:18:18 +0300

    finally fixed issue with i18n, i18n working correctly

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## This app is internationalised.
+## This app is internationalized.
 
 To see it correctly run:
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./normalize.css";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-
 import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
